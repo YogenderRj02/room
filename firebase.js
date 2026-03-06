c1cdf51b149fe21a73d3455904146a0d0c70631f
YogenderRj02 <160722007+YogenderRj02@users.noreply.github.com>
2026-03-06 10:33:41 -0800

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,13 @@
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDFRrb4VZGB6wFG2uCnqq8uwdWBriQEW1o",
+  authDomain: "room-58166.firebaseapp.com",
+  databaseURL: "https://room-58166-default-rtdb.firebaseio.com",
+  projectId: "room-58166",
+  storageBucket: "room-58166.firebasestorage.app",
+  messagingSenderId: "11760563800",
+  appId: "1:11760563800:web:b2d8f9625a44898b09bc2d"
+};
+
+firebase.initializeApp(firebaseConfig);
+var db = firebase.database();
